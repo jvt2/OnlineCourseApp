4de5aae35dac25a3ddac8c02b219b2347558b0f3
Jose Luis Vera-Tudela Durand <joseveratudela@gmail.com>
2023-07-10 19:04:07 -0500

    This is new version branch commit

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const db = require('./database/db');
 const cors = require('cors');
-
+const { body, validationResult } = require('express-validator');
 app.use(cors());
 app.use(express.json());
 app.use(passport.initialize());
@@ -41,7 +41,15 @@ const fileFilter = (req, file, cb) => {
 
 const upload = multer({ storage: storage, fileFilter: fileFilter });
 
-app.post('/register', async (req, res) => {
+app.post('/register', [
+  body('username').isString().isLength({ min: 3 }).withMessage('Username must be at leas 3 characters long'),
+  body('password').isString().isLength({ min: 8 }).withMessage('Password must be at leas 8 characters long')
+], async (req, res) => {
+  //Check for validation errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
   try {
     const { username, password } = req.body;
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
@@ -61,7 +69,15 @@ app.post('/register', async (req, res) => {
   }
 });
 
-app.post('/login', passport.authenticate('local', { session: false }), (req, res) => {
+app.post('/login', [
+  body('username').isString().withMessage('Username must be string'),
+  body('password').isString().withMessage('Password must be string')
+] ,passport.authenticate('local', { session: false }), (req, res) => {
+  // Checking for validation
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
   const token = jwt.sign({ userId: req.user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
   res.json({ token });
 });
